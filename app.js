5ab03186457a81a1a6646cb2965344e6435e327b
Student <student@localhost>
2019-09-21 10:13:41 +0300

    Убрал дублирование fixes #1

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ console.log('worked');
 const firstVote = 10;
 const secondVote = 10;
 const thirdVote = 1;
-
 let promoutersCount = 0;
 let detractorsCount = 0;
-
 if (firstVote >= 9) {
     promoutersCount = promoutersCount + 1;
 }
@@ -25,9 +23,7 @@ if (thirdVote >= 9) {
 if (thirdVote <= 6){
     detractorsCount = detractorsCount + 1;
 }
-
 const nps = (promoutersCount/3 - detractorsCount/3) * 100;
-
 console.log(nps);
 console.log(promoutersCount);
 console.log(detractorsCount);
